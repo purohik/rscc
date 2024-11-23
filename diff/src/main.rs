8023d6fed137139e6c64360a0ea8b81397b5a304
puro <purohitkrishnar@gmail.com>
2024-11-23 21:40:16 +0000

    [diff] Added starting code for longest common subsequence

---
+++ diff/src/main.rs
@@ -0,0 +1,63 @@
+use std::io;
+
+fn longest_common_subsequence(text1: String, text2: String) -> String {
+    let input1_len = text1.len();
+    let input2_len = text2.len();
+
+    let mut dp = vec![vec![0; input2_len + 1]; input1_len + 1];
+    let mut backtrack = vec![vec![0; input2_len + 1]; input1_len + 1]; // Track the optimal path
+
+    for iter1 in 1..=input1_len {
+        for iter2 in 1..=input2_len {
+            if text1.chars().nth(iter1 - 1).unwrap() == text2.chars().nth(iter2 - 1).unwrap() {
+                dp[iter1][iter2] = dp[iter1 - 1][iter2 - 1] + 1;
+                backtrack[iter1][iter2] = 3; // Diagonal: Match
+            } else if dp[iter1 - 1][iter2] >= dp[iter1][iter2 - 1] {
+                dp[iter1][iter2] = dp[iter1 - 1][iter2];
+                backtrack[iter1][iter2] = 1; // Up: Skip a character in text1
+            } else {
+                dp[iter1][iter2] = dp[iter1][iter2 - 1];
+                backtrack[iter1][iter2] = 2; // Left: Skip a character in text2
+            }
+        }
+    }
+
+    let mut iter1 = input1_len;
+    let mut iter2 = input2_len;
+    let mut lcs_result = String::new();
+
+    while iter1 > 0 && iter2 > 0 {
+        match backtrack[iter1][iter2] {
+            3 => {
+                lcs_result.push(text1.chars().nth(iter1 - 1).unwrap());
+                iter1 -= 1;
+                iter2 -= 1;
+            }
+            1 => {
+                iter1 -= 1;
+            }
+            2 => {
+                iter2 -= 1;
+            }
+            _ => unreachable!(),
+        }
+    }
+
+    lcs_result.chars().rev().collect()
+}
+
+fn main() {
+    let mut input1 = String::new();
+    let mut input2 = String::new();
+
+    io::stdin()
+        .read_line(&mut input1)
+        .expect("Failed to read input1 :/");
+
+    io::stdin()
+        .read_line(&mut input2)
+        .expect("Failed to read input2 :/");
+
+    let lcs_result = longest_common_subsequence(input1, input2);
+    println!("The longest common subsequence is: {}", lcs_result);
+}
